ce16682c8653bedbb7c369ad30355a381208cefe
Gaurav Pore <poregaurav13@gmail.com>
2021-08-20 02:31:05 +0530

    All files

---
+++ app/urlshortener/urls.py
@@ -0,0 +1,16 @@
+'''
+Urls for shortener app urlshortener/urls.py
+'''
+
+from django.urls import path
+
+# Import the home view
+from .views import home_view, redirect_url_view
+
+appname = "shortener"
+
+urlpatterns = [
+    # Home view
+    path("", home_view, name="home"),
+    path('<str:shortened_part>', redirect_url_view, name='redirect'),
+]
